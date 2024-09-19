a8466829ad3472c5b05ca0bf221562d3629204d8
MightySneed <179814389+MightySneed@users.noreply.github.com>
2024-09-19 13:44:10 +0100

    Event listeners for button clicks and arrow keys. 3 screens cycle when a b and c are pressed respectively

--- app.js
+++ app.js
@@ -2,12 +2,16 @@
 
 document.addEventListener("DOMContentLoaded", function() {
     const buttons = document.querySelectorAll(".button");
-    const stats = document.querySelectorAll(".statsdisplay")
+
+    //const stats = document.querySelectorAll(".statsdisplay")
+
     let x = 0; 
     let y = 0;
     let z = 0;
     let n = 0;
+
     //added variables for each button to see if they are counted
+
     buttons.forEach(button => {
         button.addEventListener("mouseover", function() {
             button.style.backgroundColor = "yellow"; 
@@ -22,25 +26,32 @@ document.addEventListener("DOMContentLoaded", function() {
             if (button.textContent === "A") {
                 x += 1;
                 console.log(`x is now ${x}`);
+                document.getElementById("statsdisplay").innerText =`${x.toString()} .. ${y.toString()} .. ${z.toString()} .. ${n.toString()}` ;
+
             }
         });
         button.addEventListener("click", function() {
             if (button.textContent === "B") {
                 y += 1;
                 console.log(`y is now ${y}`);
+                document.getElementById("statsdisplay").innerText =`${x.toString()} .. ${y.toString()} .. ${z.toString()} .. ${n.toString()}` ;
             }
         });
         button.addEventListener("click", function() {
             if (button.textContent === "C") {
                 z += 1;
                 console.log(`z is now ${z}`);
+                document.getElementById("statsdisplay").innerText =`${x.toString()} .. ${y.toString()} .. ${z.toString()} .. ${n.toString()}` ;
             }
         });
         button.addEventListener("click", function() {
             if (button.textContent === "D") {
                 n += 1;
                 console.log(`n is now ${n}`);
+                document.getElementById("statsdisplay").innerText =`${x.toString()} .. ${y.toString()} .. ${z.toString()} .. ${n.toString()}` ;
+
             //event listeners for clicking
+
             }
         });
     });
@@ -49,43 +60,60 @@ document.addEventListener("DOMContentLoaded", function() {
         if (event.key === "ArrowLeft") {
             x += 1;
             console.log(`x is now ${x}`);
+            document.getElementById("statsdisplay").innerText =`${x.toString()} .. ${y.toString()} .. ${z.toString()} .. ${n.toString()}` ;
         }
     });
     document.addEventListener("keydown", function(event) {
         if (event.key === "ArrowUp") {
             y += 1;
             console.log(`y is now ${y}`);
+            document.getElementById("statsdisplay").innerText =`${x.toString()} .. ${y.toString()} .. ${z.toString()} .. ${n.toString()}` ;
         }
     });
     document.addEventListener("keydown", function(event) {
         if (event.key === "ArrowRight") {
             z += 1;
             console.log(`z is now ${z}`);
+            document.getElementById("statsdisplay").innerText =`${x.toString()} .. ${y.toString()} .. ${z.toString()} .. ${n.toString()}` ;
         }
     });
     document.addEventListener("keydown", function(event) {
         if (event.key === "ArrowDown") {
             n += 1;
             console.log(`n is now ${n}`);
+            document.getElementById("statsdisplay").innerText =`${x.toString()} .. ${y.toString()} .. ${z.toString()} .. ${n.toString()}` ;
+
             //event listeners for keydowns
+
         }
     });
+    document.addEventListener("keydown", function(event) {
+
+        if (event.key === "a") {
+            console.log(`Open StartScreen`);
+            document.getElementById("startscreen").style.maxHeight = `1000px`;
+            document.getElementById("gamescreen").style.maxHeight = `0px`;
+            document.getElementById("endscreen").style.maxHeight = `0px`;
+        }});
+    document.addEventListener("keydown", function(event) {
+        if (event.key === "b") {
+            console.log(`Open GameScreen`);
+            document.getElementById("gamescreen").style.maxHeight = `1000px`;
+            document.getElementById("startscreen").style.maxHeight = `0px`;
+            document.getElementById("endscreen").style.maxHeight = `0px`;
+
+        }});
+    document.addEventListener("keydown", function(event) {
+        if (event.key === "c") {
+            console.log(`Open End Screen`);
+            document.getElementById("endscreen").style.maxHeight = `1000px`;
+            document.getElementById("startscreen").style.maxHeight = `0px`;
+            document.getElementById("gamescreen").style.maxHeight = `0px`;
+
+            //event listeners for screen cycling 
+
+        }});
 });
 
 
-stats.addEventListener("keydown").textContent(`${x}`)
-//me trying to make "Here be buttons start dipslaying X value (A button or left button click)"
-
-const button1  = document.getElementsByClassName(`button`)[0]
-const button2 = document.getElementsByClassName(`button`)[1]
-const button3 = document.getElementsByClassName(`button`)[2]
-const button4 = document.getElementsByClassName(`button`)[3]
-const button5 = document.getElementsByClassName(`button`)[4]
-button1.textContent = `${x}`
-button2.textContent = `${y}`
-button3.textContent = `${z}`
-button4.textContent = `${n}`
-button5.textContent = `${x+y+z+n}`
-
-//Me trying to make buttons display click values
 
