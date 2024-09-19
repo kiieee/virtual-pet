36b1c5b2891c5cdade62535fc5843f0ea094ba44
MightySneed <179814389+MightySneed@users.noreply.github.com>
2024-09-19 16:00:57 +0100

    Most recent full build

--- app.js
+++ app.js
@@ -1,4 +1,201 @@
+let pet 
+
 //Main button functionality
+//Animal class 4 variables HPoints, Hunger, APoints, Thirst.
+//5th variable in subclass differs for each creature.
+//All values set to 200 
+
+class Animal {
+    constructor(name){
+        this.name = name
+        this.healthPoints = 200
+        this.hunger = 200
+        this.actionPoints = 200
+        this.thirsty = 200
+    }
+    drink() {
+        this.healthPoints = (this.health + 15 >= 200) ? 200 : this.health + 15;
+        this.thirsty = (this.thirsty + 15 >= 200) ? 200 : this.thirsty + 15;
+    }
+    eats(){
+        this.healthPoints = (this.health + 25 >= 200) ? 200 : this.health + 25;
+        this.thirsty -= 5;
+        this.hunger = (this.hunger + 20 >= 200) ? 200 : this.hunger + 20;
+    }
+}
+
+class Dragon extends Animal{
+    constructor(name){
+        super(name); 
+        this.happinessPoints = 200;
+    }
+    burnVillage() {
+        this.actionPoints -=20
+        this.happinessPoints = (this.happinessPoints + 25 >= 200) ? 200 : this.happinessPoints + 25;
+        this.hunger = (this.hunger + 10 >= 200) ? 200 : this.hunger + 10;
+
+    giveGold(); {
+        this.actionPoints = (this.actionPoints + 25 >= 200) ? 200 : this.actionPoints +35;
+        this.healthPoints = (this.healthPoints + 25 >= 200) ? 200 : this.healthPoints + 5;
+    }
+
+    action(activity); {
+        switch(activity) {
+            case 'Drink':
+                this.drink();
+                console.log("Refreshing!")
+                break;
+            case 'Eat':
+                this.eats();
+                console.log("Yummy!")
+                break;
+            case 'Burn Village':
+                this.burnVillage();
+                break;
+            case 'Give Gold':
+                this.giveGold();
+                break;
+            default:
+                break;
+        }
+    }
+
+    isAlive(); {
+        if (this.healthPoints <= 0) {
+            msg.textContent = `${this.name} was too unhealthy, and has died.`;
+        }
+
+        if (this.hunger <= 0) {
+            msg.textContent = `${this.name} didnt burn enough villages for food and has starved `;
+        }
+    
+        if (this.thirsty <= 0) {
+            msg.textContent = `${this.name} died from the lack of water`;
+        }
+    
+        if (this.happinessPoints <= 0) {
+            msg.textContent = `${this.name} died of misery`;
+        }
+
+        if (this.actionPoints <= 0) {
+            msg.textContent = `${this.name} became lazy and died`;
+        }
+    }
+    
+}}
+
+class Manticore extends Animal{
+    constructor(name){
+        super(name);
+        this.savageryPoints = 200;
+    }
+stingAttack(){
+    this.actionPoints -=20
+    this.savageryPoints = (this.savageryPoints + 25 >= 200) ? 200 : this.savageryPoints + 25;
+    this.hunger = (this.hunger + 10 >= 200) ? 200 : this.hunger + 10;
+}
+playYarn(){
+    this.actionPoints = (this.actionPoints + 25 >= 200) ? 200 : this.actionPoints +35;
+    this.healthPoints = (this.healthPoints + 25 >= 200) ? 200 : this.healthPoints + 5;
+}
+
+action(activity) {
+    switch(activity) {
+        case 'Drink':
+            this.drink();
+            break;
+        case 'Eat':
+            this.eats();
+            break;
+        case 'Sting Attack':
+            this.stingAttack();
+            break;
+        case 'Play with Yarn':
+            this.playYarn();
+            break;
+        default:
+            break;
+    }
+}
+
+isAlive() {
+    if (this.healthPoints <= 0) {
+        msg.textContent = `${this.name} was too unhealthy, and has died.`;
+    }
+
+    if (this.hunger <= 0) {
+        msg.textContent = `${this.name} didnt trap enough humans for food and has starved `;
+    }
+
+    if (this.thirsty <= 0) {
+        msg.textContent = `${this.name} died from the lack of blood`;
+    }
+
+    if (this.savageryPoints <= 0) {
+        msg.textContent = `${this.name} died of misery`;
+    }
+
+    if (this.actionPoints <= 0) {
+        msg.textContent = `${this.name} became lazy and died`;
+    }
+}
+}
+
+class Centaur extends Animal{
+    constructor(name){
+        super(name)
+        this.moodPoints = 200;
+    }
+shootArrow() {
+    this.actionPoints -=20
+    this.moodPoints = (this.moodPoints + 25 >= 200) ? 200 : this.moodPoints + 25;
+    this.hunger = (this.hunger + 10 >= 200) ? 200 : this.hunger + 10;
+}
+kickHooves() {
+    this.actionPoints = (this.actionPoints + 25 >= 200) ? 200 : this.actionPoints +35;
+    this.healthPoints = (this.healthPoints + 25 >= 200) ? 200 : this.healthPoints + 5;
+}
+action(activity) {
+    switch(activity) {
+        case 'Drink':
+            this.drink();
+            break;
+        case 'Eat':
+            this.eats();
+            break;
+        case 'Shoot Arrow':
+            this.shootArrow();
+            break;
+        case 'Kick Hooves':
+            this.kickHooves();
+            break;
+        default:
+            break;
+    }
+}
+
+isAlive() {
+    if (this.healthPoints <= 0) {
+        msg.textContent = `${this.name} was too unhealthy, and has died.`;
+    }
+
+    if (this.hunger <= 0) {
+        msg.textContent = `${this.name} didnt burn enough villages for food and has starved `;
+    }
+
+    if (this.thirsty <= 0) {
+        msg.textContent = `${this.name} died from the lack of water`;
+    }
+
+    if (this.happinessPoints <= 0) {
+        msg.textContent = `${this.name} died of misery`;
+    }
+
+    if (this.actionPoints <= 0) {
+        msg.textContent = `${this.name} became lazy and died`;
+    }
+}}
+
 
 document.addEventListener("DOMContentLoaded", function() {
     const buttons = document.querySelectorAll(".button");
@@ -12,6 +209,21 @@ document.addEventListener("DOMContentLoaded", function() {
 
     //added variables for each button to see if they are counted
 
+    const startbutton = document.getElementById("startbutt")
+
+    startbutton.addEventListener("click", function() {
+        pet = new Animal("Billy")
+        let animalname = document.getElementById("name").value 
+        let animalclass = document.getElementById("animal").value
+        console.log(`${animalname} ${animalclass}`)
+        console.log(`Open GameScreen`);
+        document.getElementById("gamescreen").style.maxHeight = `1000px`;
+        document.getElementById("startscreen").style.maxHeight = `0px`;
+        document.getElementById("endscreen").style.maxHeight = `0px`;
+
+    })
+
+
     buttons.forEach(button => {
         button.addEventListener("mouseover", function() {
             button.style.backgroundColor = "yellow"; 
@@ -22,11 +234,13 @@ document.addEventListener("DOMContentLoaded", function() {
             button.style.backgroundColor = ""; 
         });
 
+
         button.addEventListener("click", function() {
             if (button.textContent === "A") {
                 x += 1;
                 console.log(`x is now ${x}`);
                 document.getElementById("statsdisplay").innerText =`${x.toString()} .. ${y.toString()} .. ${z.toString()} .. ${n.toString()}` ;
+                action = eats()
 
             }
         });
@@ -35,6 +249,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 y += 1;
                 console.log(`y is now ${y}`);
                 document.getElementById("statsdisplay").innerText =`${x.toString()} .. ${y.toString()} .. ${z.toString()} .. ${n.toString()}` ;
+                action = drinks()
             }
         });
         button.addEventListener("click", function() {
@@ -116,4 +331,28 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 
+let food = 100; 
+let timer = 1; 
+const timerDisplay = document.getElementById('timer-display');
+const foodDisplay = document.getElementById('food-count');
+const foodbar = document.getElementById(`foodbar`)
 
+function updateTimer() {
+    timer--;
+    timerDisplay.textContent = timer;
+
+    if (timer === 0) {
+        food -= 3; // this allows you to change the food when needed
+        if (food < 0) {
+            food = 0; 
+        }
+        timer = 5; // this is the timer change as needed
+        foodDisplay.textContent = food;
+        // howlong = food;
+        // foodbar.style.width = howlong
+        // tried to make healthbar scale with time
+    }
+}
+
+
+setInterval(updateTimer, 1000);
